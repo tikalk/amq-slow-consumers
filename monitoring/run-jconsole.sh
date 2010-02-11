41ecd009f56e8014b122e15d05f778f6f0d74be7
Shalom Kazaz <shalom@tikalk.com>
2010-02-11 08:48:48 +0200

    README

--- monitoring/run-jconsole.sh
+++ monitoring/run-jconsole.sh
@@ -2,7 +2,7 @@
 
 exec /usr/local/jdk/jdk/bin/jconsole \
             -J-Djava.util.logging.config.file=jconsole-logging.properties \
-            -J-Djavax.net.debug=ssl \
+            -J-Djavax.net.debug=all \
             -J-Djavax.net.ssl.trustStore=/devel/tikal-fuse/amq-slow-consumers/monitoring/jconsole.ketstore \
             -J-Djavax.net.ssl.trustStorePassword=changeit \
             -J-Djavax.net.ssl.keyStore=/devel/tikal-fuse/amq-slow-consumers/monitoring/jconsole.ketstore  \
